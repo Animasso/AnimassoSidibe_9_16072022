8d0ab93c7dbf012b26d5cc8eb21e82353a308888
Animasso Sidibe <asidibe017@gmail.com>
2022-07-31 14:45:01 +0200

    premier'

---
+++ Billed-app-FR-Front/src/containers/NewBill.js
@@ -0,0 +1,118 @@
+import { ROUTES_PATH } from "../constants/routes.js";
+import Logout from "./Logout.js";
+
+//TODO faire une div qui recois le p et mettre le p en display none
+// const divMgs = document.createElement("div");
+// colHalf.appendChild(divMgs);
+// const falseMgs = document.createElement("p");
+// falseMgs.classList.add("wrongType");
+// divMgs.appendChild(falseMgs);
+// falseMgs.textContent = "Document non supporté";
+// falseMgs.style.color = "red";
+// falseMgs.style.opacity = 0;
+
+export default class NewBill {
+  constructor({ document, onNavigate, store, localStorage }) {
+    this.document = document;
+    this.onNavigate = onNavigate;
+    this.store = store;
+    const formNewBill = this.document.querySelector(
+      `form[data-testid="form-new-bill"]`
+    );
+    formNewBill.addEventListener("submit", this.handleSubmit);
+    const file = this.document.querySelector(`input[data-testid="file"]`);
+    file.addEventListener("change", this.handleChangeFile);
+    this.fileUrl = null;
+    this.fileName = null;
+    this.billId = null;
+    new Logout({ document, localStorage, onNavigate });
+  }
+
+  //here for the issue about testing the extension
+
+  handleChangeFile = (e) => {
+    e.preventDefault();
+    const wrongType = document.querySelector(".wrongType");
+    console.log("colHalf:", wrongType);
+    wrongType.style.opacity = 0;
+    const file = this.document.querySelector(`input[data-testid="file"]`)
+      .files[0];
+    const filePath = e.target.value.split(/\\/g);
+    console.log("filePath :", filePath);
+    const fileName = filePath[filePath.length - 1];
+    console.log("fileName:", fileName);
+    const extension = fileName.split(".")[1];
+    console.log("extension:", extension);
+    if (extension === "png" || extension === "jpeg " || extension === "jpg") {
+      console.log(`good file`);
+      // falseMgs.style.opacity = 0;
+      // console.log("divMgs:", divMgs);
+      const formData = new FormData();
+      const email = JSON.parse(localStorage.getItem("user")).email;
+      formData.append("file", file);
+      formData.append("email", email);
+      this.store
+        .bills()
+        .create({
+          data: formData,
+          headers: {
+            noContentType: true,
+          },
+        })
+        .then(({ fileUrl, key }) => {
+          console.log(fileUrl);
+          this.billId = key;
+          this.fileUrl = fileUrl;
+          this.fileName = fileName;
+        })
+        .catch((error) => console.error(error));
+    } else {
+      this.document.querySelector(`input[data-testid="file"]`).value = null;
+      wrongType.style.opacity = 1;
+      wrongType.style.color = "red";
+      console.log(`mauvais type`);
+    }
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(
+      'e.target.querySelector(`input[data-testid="datepicker"]`).value',
+      e.target.querySelector(`input[data-testid="datepicker"]`).value
+    );
+
+    const email = JSON.parse(localStorage.getItem("user")).email;
+    const bill = {
+      email,
+      type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
+      name: e.target.querySelector(`input[data-testid="expense-name"]`).value,
+      amount: parseInt(
+        e.target.querySelector(`input[data-testid="amount"]`).value
+      ),
+      date: e.target.querySelector(`input[data-testid="datepicker"]`).value,
+      vat: e.target.querySelector(`input[data-testid="vat"]`).value,
+      pct:
+        parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) ||
+        20,
+      commentary: e.target.querySelector(`textarea[data-testid="commentary"]`)
+        .value,
+      fileUrl: this.fileUrl,
+      fileName: this.fileName,
+      status: "pending",
+    };
+    this.updateBill(bill);
+    this.onNavigate(ROUTES_PATH["Bills"]);
+  };
+
+  // not need to cover this function by tests
+  updateBill = (bill) => {
+    if (this.store) {
+      this.store
+        .bills()
+        .update({ data: JSON.stringify(bill), selector: this.billId })
+        .then(() => {
+          this.onNavigate(ROUTES_PATH["Bills"]);
+        })
+        .catch((error) => console.error(error));
+    }
+  };
+}
